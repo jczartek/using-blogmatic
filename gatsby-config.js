dcd58e18bfb36fc0a9dedacac870570dd93edd54
Jakub Czartek <kuba@linux.pl>
2022-06-12 20:40:56 +0200

    title from metasite

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,11 @@
 module.exports = {
     siteMetadata: {
+        title: 'Jakub Czartek - Using Blogmatic',
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
         'gatsby-plugin-postcss',
+        'gatsby-plugin-image',
+        'gatsby-plugin-sharp',
     ],
 }
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,13 +1,29 @@
 import * as React from "react";
-import { Link } from "gatsby";
+import { Link, useStaticQuery, graphql } from "gatsby";
 
 const Nav = () => {
+
+  const data = useStaticQuery(graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`);
+
   return (
     <div className="w-full flex justify-center items-center bg-cover bg-nav-image xl:h-96 bg-gradient-to-r mb-2 shadow">
       <nav className="flex flex-col items-center justify-center">
-        <h4 className="font-medium text-zinc-100 tracking-wide antialiased text-2xl xl:text-5xl hover:-translate-y-1 hover:scale-110 hover:text-sky-300 duration-300">
-          Jakub Czartek | Using blogmatic
-        </h4>
+        <header className="font-medium
+                         text-zinc-100 
+                         tracking-wide 
+                         antialiased 
+                         text-2xl xl:text-5xl 
+                         hover:-translate-y-1 hover:scale-110 hover:text-sky-300 duration-300">
+          {data.site.siteMetadata.title}
+        </header>
         <ul className="flex flex-row text-zinc-100 text-2xl xl:text-3xl ">
           <li className="p-5 hover:-translate-y-1 hover:scale-110 hover:text-sky-300 duration-300">
             <Link to="/">Posty</Link>
