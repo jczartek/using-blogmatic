229fa9023a247054a37ecb332d50a3b6114760c8
Jakub Czartek <kuba@linux.pl>
2022-06-14 06:58:12 +0200

    mdx
    React down to version 17

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,5 +7,13 @@ module.exports = {
         'gatsby-plugin-postcss',
         'gatsby-plugin-image',
         'gatsby-plugin-sharp',
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: `blog`,
+                path: `${__dirname}/blog`,
+            }
+        },
+        "gatsby-plugin-mdx",
     ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,38 @@
 import * as React from "react";
+import { graphql } from "gatsby";
 import Layout from "../components/layout";
 
-export default function IndexPage() {
+const IndexPage = ({ data }) => {
   return (
     <Layout pageTitle="using blogmatic - Home">
-      <h1 className="text-3xl font-bold underline">
-      </h1>
+      <div className="posts w-full">
+        {data.allMdx.nodes.map((node) => {          
+          return (
+            <div className="flex flex-col mb-2" key={node.id}>
+              <div className="flex flex-col p-6 bg-white">
+                <div className="text-gray-600 text-sm">Data opublikowania: {node.frontmatter.date}</div>
+                <div className="text-zinc-500 font-light text-xl">{node.frontmatter.title}</div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </Layout>
   );
-}
+};
+
+export const querySite = graphql`
+query {
+  allMdx (sort: {fields: frontmatter___date, order: DESC}){
+    nodes {
+      frontmatter {
+        date(formatString: "DD MMMM, YYYY", locale: "pl")
+        title
+        author
+      }
+      id
+    }
+  }
+}`
+
+export default IndexPage;
