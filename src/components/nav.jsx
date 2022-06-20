40b494e80ca263f2ef246ac9e7e786cc56618b9c
Jakub Czartek <kuba@linux.pl>
2022-06-20 19:34:16 +0200

    fix css

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -14,17 +14,17 @@ const Nav = () => {
 `);
 
   return (
-    <div className="w-full flex justify-center items-center bg-cover bg-nav-image xl:h-96 bg-gradient-to-r mb-2 shadow">
+    <div className="grid grid-cols-1 bg-cover bg-nav-image xl:h-96 h-56 mb-2 shadow rounded-b">
       <nav className="flex flex-col items-center justify-center">
         <header className="font-medium
                          text-slate-200
                          tracking-wide 
                          antialiased 
-                         text-2xl xl:text-5xl 
+                         text-xl xl:text-5xl 
                          hover:-translate-y-1 hover:scale-110 hover:text-sky-300 duration-300">
           {data.site.siteMetadata.title}
         </header>
-        <ul className="flex flex-row text-zinc-100 text-2xl xl:text-3xl ">
+        <ul className="flex flex-row text-zinc-100 text-xl xl:text-3xl ">
           <li className="p-5 hover:-translate-y-1 hover:scale-110 hover:text-sky-300 duration-300">
             <Link to="/">Posty</Link>
           </li>
